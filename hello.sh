c88aaf50c8fe0b5aaf0184906b19c3bc0ca74b07
root <root@ip-172-31-37-127.eu-central-1.compute.internal>
2026-06-19 14:21:53 +0000

    my build was successfull

--- hello.sh
+++ hello.sh
@@ -2,3 +2,4 @@ echo "my first build done successfully."
 echo "Triggered at $(date)"
 echo "I am a Cloud and Devops Engineer
 I am a Cloud and Devops Engineer having 6 years of experience"
+echo "My build was successfull"
